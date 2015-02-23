60a5461ae4246b1476138bbea6a48c10834183a1
Elliott Hindman <portokaliajapan@gmail.com>
2015-02-23 10:24:27 -0800

    Formats js functions better

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
+count = 0;
+
 $(document).ready(function() {
-var count = 0;
-function unbindButton() {
-    $("img").unbind('click').one("click", function() {
-        $(this).fadeOut('fast');
-        count++;
-        if (count >=8) {
-         $('.success').removeClass('hide');
-        }
-    });
-}
+    function unbindButton() {
+        $("img").unbind('click').one("click", function() {
+            $(this).fadeOut('fast');
+            count++;
+            if (count >=8) {
+             $('.success').removeClass('hide');
+            }
+        });
+    }
 
-unbindButton();
-
-$(".btn-success").on("click", function() {
-    $('.success').addClass('hide');
-    $('img').show();
     unbindButton();
-    count=0;
+
+    $(".btn-success").on("click", function() {
+        $('.success').addClass('hide');
+        $('img').show();
+        unbindButton();
+        count=0;
+    });
 });
- });
